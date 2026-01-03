899659afb7b6b5f5fe2a77cb93da6dccabb0d755
Navir Zalgenos <ghub@reodus.dev>
2026-01-03 10:45:32 -0500

    Install Guest Additions in a Linux VM in VBox

--- vbox_ga.py
+++ vbox_ga.py
@@ -23,6 +23,11 @@ sudo usermod -aG vboxsf $USER
 ### With this, we now can adjust screen resolution/size
 ### Make sure to enable clipboard and drag/drop settings in vbox
 
+###Another way and more updated/more eaier way
+sudo apt update
+sudo apt install build-essential dkms linux-headers-$(uname -r) virtualbox-guest-utils virtualbox-guest-x11
+#then reboot
+
 ### In case of error in running VMs because of KVM error, sometimes because of linux newer kernels updated
 ### Run this every time before powering on a vm in vbox, there is a method to make it persistent but this is much safer
 sudo modprobe -r kvm_amd kvm      # If using AMD CPU:
